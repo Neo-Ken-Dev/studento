06e32037d68098159437a2997ba7af7755bf3a59
ken l <neokendev@gmail.com>
2021-09-27 15:34:58 +0200

    erro crediental traslate

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,13 +5,12 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterType;
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
@@ -62,7 +61,13 @@ class UserController extends AbstractController
     /**
      * @Route("/connexion", name="login")
      */
-    public function login() {return $this->render("user/login.html.twig");}
+    public function login(AuthenticationUtils $authenticationUtils) {
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render("user/login.html.twig", ['error'=>$error]);
+
+    }
     /**
      * @Route("/logout", name="logout")
      */
