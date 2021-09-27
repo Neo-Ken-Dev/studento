254c15d6fb86ddcab4ecf90f6ec1dc2655ce6e67
ken l <neokendev@gmail.com>
2021-09-27 14:38:54 +0200

    create role student and teacher

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -37,8 +37,29 @@ class UserFixtures extends Fixture
         $user2->setEmail("fenestre.dorian@free.fr");
         $password = $this->encoder->encodePassword($user2, "12345");
         $user2->setPassword($password);
-        $user2->setRoles(array("ROLE_USER"));
+        $user2->setRoles(array("ROLE_ADMIN"));
         $manager->persist($user2);
+
+        $userStudent = new User();
+        $userStudent->setLastname("student");
+        $userStudent->setFirstname("o");
+        $userStudent->setUsername("studento");
+        $userStudent->setEmail("student.o@free.fr");
+        $password = $this->encoder->encodePassword($userStudent, "1234");
+        $userStudent->setPassword($password);
+        $userStudent->setRoles(array("ROLE_STUDENT"));
+        $manager->persist($userStudent);
+
+        $userTeacher = new User();
+        $userTeacher->setLastname("Teacher");
+        $userTeacher->setFirstname("o");
+        $userTeacher->setUsername("teachero");
+        $userTeacher->setEmail("teacher.o@free.fr");
+        $password = $this->encoder->encodePassword($userTeacher, "1234");
+        $userTeacher->setPassword($password);
+        $userTeacher->setRoles(array("ROLE_TEACHER"));
+        $manager->persist($userTeacher);
+
 
         $manager->flush();
     }
